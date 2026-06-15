00c6d592e69e394b8550a96c4bdee13aef32bdba
boris-valero <157314735+boris-valero@users.noreply.github.com>
2026-06-15 11:14:43 +0200

    feat: load dashboard tour

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,10 +6,12 @@ namespace OCA\Intros\AppInfo;
 
 use OCA\Files\Event\LoadAdditionalScriptsEvent;
 use OCA\Intros\Listeners\FilesLoadAdditionalScriptsListener;
+use OCA\Intros\Listeners\DashboardLoadAdditionalScriptsListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'intros';
@@ -21,6 +23,7 @@ class Application extends App implements IBootstrap {
 	#[\Override]
 	public function register(IRegistrationContext $context): void {
 		$context->registerEventListener(LoadAdditionalScriptsEvent::class, FilesLoadAdditionalScriptsListener::class);
+		$context->registerEventListener(BeforeTemplateRenderedEvent::class, DashboardLoadAdditionalScriptsListener::class);
 	}
 
 	#[\Override]
